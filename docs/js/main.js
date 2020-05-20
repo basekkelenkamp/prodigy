23b80b0b27c1965f79d5c69e3adfa2c451aac1c2
Bas <57452503+basekkelenkamp@users.noreply.github.com>
2020-05-20 11:34:20 +0200

    commit enemies pad wip

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,35 +4,57 @@ class Game {
         this.enemy1 = new Enemy(1);
         this.enemy2 = new Enemy(2);
         this.enemy3 = new Enemy(3);
+        this.enemy4 = new Enemy(5);
         this.gameLoop();
     }
     gameLoop() {
         this.enemy1.move();
         this.enemy2.move();
         this.enemy3.move();
+        this.enemy4.move();
         requestAnimationFrame(() => this.gameLoop());
     }
 }
 window.addEventListener("load", () => new Game());
 class Enemy {
-    constructor(strength) {
+    constructor(level) {
+        this.xspeed = 0;
+        this.yspeed = 0;
+        this.state = 0;
         this.x = 0;
         this.y = 0;
-        this.strengthLevel = strength;
-        this.healthPoints = strength * 100;
-        this.damage = strength * 5;
-        this.speed = 0.75 / this.strengthLevel;
+        this.strength = level;
+        this.healthPoints = level * 100;
+        this.damage = level * 5;
+        this.xspeed = 0.75 / this.strength;
+        this.yspeed = 0;
         this.element = document.createElement("enemy");
         let game = document.getElementsByTagName("game")[0];
         game.appendChild(this.element);
-        this.element.style.filter = `hue-rotate(${this.strengthLevel * 90}deg)`;
+        this.element.style.filter = `hue-rotate(${level * 60}deg)`;
         this.healthBar = document.createElement("healthbar");
         this.healthBar.innerHTML = `${this.healthPoints}HP`;
         this.element.appendChild(this.healthBar);
     }
     move() {
-        this.x += this.speed;
-        this.y = this.strengthLevel * 100;
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+        switch (this.state) {
+            case 0:
+                if (this.x > 200) {
+                    this.yspeed = 0.75 / this.strength;
+                    this.xspeed = 0;
+                    this.state = 1;
+                }
+                break;
+            case 1:
+                if (this.y > 500) {
+                    this.xspeed = 0.75 / this.strength;
+                    this.yspeed = 0;
+                    this.state = 2;
+                }
+                break;
+        }
         this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
     }
 }
--- src/objects/enemies/enemy.ts
+++ src/objects/enemies/enemy.ts
@@ -3,28 +3,31 @@ class Enemy {
     element : HTMLElement
     healthBar : HTMLElement
 
-    strengthLevel : number
+    strength : number
     healthPoints : number
     damage : number
-    speed : number
+    xspeed : number = 0
+    yspeed : number = 0
+    state : number = 0
 
     x : number = 0
     y : number = 0
 
-    constructor(strength : number){
+    constructor(level : number){
 
         //Initialize enemy: || STRENGTH || HP || DAMAGE || SPEED ||
-        this.strengthLevel = strength
-        this.healthPoints = strength*100
-        this.damage = strength*5
-        this.speed = 0.75 / this.strengthLevel 
+        this.strength = level
+        this.healthPoints = level*100
+        this.damage = level*5
+        this.xspeed = 0.75 / this.strength
+        this.yspeed = 0
 
 
         //Create enemy
         this.element = document.createElement("enemy")
         let game = document.getElementsByTagName("game")[0]
         game.appendChild(this.element)
-        this.element.style.filter = `hue-rotate(${this.strengthLevel*90}deg)`
+        this.element.style.filter = `hue-rotate(${level*60}deg)`
 
         //Create HP bar
         this.healthBar = document.createElement("healthbar")
@@ -34,9 +37,33 @@ class Enemy {
     }
 
     move(){
-        this.x += this.speed
-        this.y = this.strengthLevel*100
 
+        this.x += this.xspeed
+        this.y += this.yspeed
+         
+        switch (this.state) {
+            case 0 : 
+                if (this.x > 200) {
+                    this.yspeed = 0.75 / this.strength
+                    this.xspeed = 0
+                    this.state = 1
+                }
+            break;
+
+            case 1 :
+                if (this.y > 500) {
+                    this.xspeed = 0.75 / this.strength
+                    this.yspeed = 0
+                    this.state = 2
+                }
+                break;
+
+        }
+    
+
+
+
+        
         this.element.style.transform = `translate(${this.x}px, ${this.y}px)`
 
     }
