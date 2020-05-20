8452837c5ee2b935c88d6455899c9d6fa50b0072
kkarta30 <58032093+kkarta30@users.noreply.github.com>
2020-05-20 15:21:21 +0200

    castle

--- src/objects/enemies/castle.ts
+++ src/objects/enemies/castle.ts
@@ -3,10 +3,7 @@ class Castle {
     element : HTMLElement
     healthBar : HTMLElement
 
-    // strengthLevel : number
     healthPoints : number = 1000
-    // damage : number
-    // speed : number
 
     x : number = 500
     y : number = 0
@@ -14,19 +11,10 @@ class Castle {
     castleImg : number = 1
 
     constructor(){
-
-        //Initialize enemy: || STRENGTH || HP || DAMAGE || SPEED ||
-        // this.strengthLevel = strength
-        // this.healthPoints = strength*100
-        // this.damage = strength*5
-        // this.speed = 0.75 / this.strengthLevel 
-
-
         //Create castle
         this.element = document.createElement("castle")
         let game = document.getElementsByTagName("game")[0]
         game.appendChild(this.element)
-        // this.element.style.filter = `hue-rotate(${this.strengthLevel*90}deg)`
         this.element.style.transform = `translate(${this.x}px, ${this.y}px)`
 
         //Create HP bar
@@ -43,7 +31,6 @@ class Castle {
 
     break(){
         
-        //als hp onder 1000 komt
         this.castleImg ++
         if (this.castleImg > 6) {
             this.castleImg = 1
@@ -53,16 +40,9 @@ class Castle {
             this.healthPoints = 1000
         }
         this.healthBar.innerHTML = `${this.healthPoints}HP`
-        //background-image: `url(../../src/assets/images/castle/castle${i}.png)`;
         this.element.style.backgroundImage = `url(../../src/assets/images/castle/castle${this.castleImg}.png)`;
 
     }
-    // move(){
-    //     this.x += this.speed
-    //     this.y = this.strengthLevel*100
 
-    //     this.element.style.transform = `translate(${this.x}px, ${this.y}px)`
-
-    // }
 
 }
