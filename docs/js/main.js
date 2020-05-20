a71362690222513c9e06d24a69fd4ba03c797ec6
kkarta30 <58032093+kkarta30@users.noreply.github.com>
2020-05-20 14:26:27 +0200

    Castle
    Castle toegevoegd

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,6 +4,7 @@ class Game {
         this.enemy1 = new Enemy(1);
         this.enemy2 = new Enemy(2);
         this.enemy3 = new Enemy(3);
+        this.castle = new Castle();
         this.gameLoop();
     }
     gameLoop() {
@@ -14,6 +15,34 @@ class Game {
     }
 }
 window.addEventListener("load", () => new Game());
+class Castle {
+    constructor() {
+        this.healthPoints = 1000;
+        this.x = 500;
+        this.y = 0;
+        this.castleImg = 1;
+        this.element = document.createElement("castle");
+        let game = document.getElementsByTagName("game")[0];
+        game.appendChild(this.element);
+        this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
+        this.healthBar = document.createElement("healthbar");
+        this.healthBar.innerHTML = `${this.healthPoints}HP`;
+        this.element.appendChild(this.healthBar);
+        this.element.addEventListener("click", () => this.break());
+    }
+    break() {
+        this.castleImg++;
+        if (this.castleImg > 6) {
+            this.castleImg = 1;
+        }
+        this.healthPoints -= 200;
+        if (this.healthPoints < 0) {
+            this.healthPoints = 1000;
+        }
+        this.healthBar.innerHTML = `${this.healthPoints}HP`;
+        this.element.style.backgroundImage = `url(../../src/assets/images/castle/castle${this.castleImg}.png)`;
+    }
+}
 class Enemy {
     constructor(strength) {
         this.x = 0;
--- src/game.ts
+++ src/game.ts
@@ -5,12 +5,14 @@ class Game {
     enemy1 : Enemy
     enemy2 : Enemy
     enemy3 : Enemy
+    castle : Castle
 
     constructor(){
         console.log("game created!")
         this.enemy1 = new Enemy(1)
         this.enemy2 = new Enemy(2)
         this.enemy3 = new Enemy(3)
+        this.castle = new Castle()
 
 
         this.gameLoop()
@@ -20,6 +22,7 @@ class Game {
         this.enemy1.move()
         this.enemy2.move()
         this.enemy3.move()
+        
 
         requestAnimationFrame(() => this.gameLoop())
     }
